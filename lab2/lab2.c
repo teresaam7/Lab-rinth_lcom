eda4eadc66f454a0751bc149b747643f40a1d155
Beatriz Bernardo <up202206097@up.pt>
2024-03-27 17:16:36 +0000

    timer_test_read_config

--- lab2/lab2.c
+++ lab2/lab2.c
@@ -30,10 +30,14 @@ int main(int argc, char *argv[]) {
 }
 
 int(timer_test_read_config)(uint8_t timer, enum timer_status_field field) {
-  /* To be implemented by the students */
-  printf("%s is not yet implemented!\n", __func__);
-
-  return 1;
+  uint8_t config;
+  if(timer_get_conf(timer, &config)!=0){
+    return 1;
+  }
+  if(timer_display_conf(timer, config, field)!=0){ 
+    return 1;
+  }
+  return 0;
 }
 
 int(timer_test_time_base)(uint8_t timer, uint32_t freq) {
--- lab2/timer.c
+++ lab2/timer.c
@@ -4,8 +4,9 @@
 #include <stdint.h>
 
 #include "i8254.h"
-#include <lcf.h>
-#include <syslib.h>
+//#include <lcf.h>
+//#include <minix/syslib.h>
+//#include <syslib.h>
 
 int hookid=0;
 int counter=0;
@@ -44,7 +45,7 @@ int (timer_get_conf)(uint8_t timer, uint8_t *st) {
   }
   uint8_t readbackcommand= (TIMER_RB_CMD | TIMER_RB_COUNT_ | TIMER_RB_SEL(timer));
 
-  if (util_sys_outb(TIMER_CTRL, readbackcommand) != 0) {
+  if (sys_outb(TIMER_CTRL, readbackcommand) != 0) { 
     return 1;
   }
 
@@ -52,13 +53,43 @@ int (timer_get_conf)(uint8_t timer, uint8_t *st) {
   if (util_sys_inb(timer_port, st) != 0) {
       return 1; 
   }
+
   return 0; 
 }
 
 int (timer_display_conf)(uint8_t timer, uint8_t st,
                         enum timer_status_field field) {
-  /* To be implemented by the students */
-  printf("%s is not yet implemented!\n", __func__);
-
-  return 1;
+  union timer_status_field_val status; 
+  uint8_t mode; //nao pode ser criado dentro do switch
+  switch(field){
+    case tsf_all:
+      status.byte=st;
+    break;
+    case tsf_initial:
+      mode=(st>>4) & 0x03;
+      if(mode==1){ 
+        status.in_mode=LSB_only;}
+      else if(mode==2){ 
+        status.in_mode=MSB_only;}
+      else if(mode==3){ 
+        status.in_mode=MSB_after_LSB;}
+      else{ 
+        status.in_mode=INVAL_val;}
+    break;
+    case tsf_mode:
+      mode=(st>>1) & 0x07;
+      if(mode==6){ status.count_mode=2;} //mode 2=X10    010 e 110
+      else if(mode==7){ status.count_mode=3;} //mode 3=X11   111 e 011
+      else status.count_mode=mode;
+    break;
+    case tsf_base:
+       status.bcd=st&TIMER_BCD; //true se é BCD 
+    break;
+    default:
+       return 1;
+  }
+  if (timer_print_config(timer, field, status) != 0){
+     return 1;
+  }
+  return 0;
 }
